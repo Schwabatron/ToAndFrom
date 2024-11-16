131b2486ae74615b3e8feb5effc869cbbedcffc0
nathan <nstrunk@albany.edu>
2024-11-16 17:42:13 -0500

    Created zombie prefab, animations and basic AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,6 +6,12 @@ public class EnemyAI : MonoBehaviour
     public GameObject player;
     public float speed = 5.0f;
     private float distance;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -13,5 +19,8 @@ public class EnemyAI : MonoBehaviour
         Vector2 direction = player.transform.position - transform.position;
         
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        
+        animator.SetFloat("Move X", direction.x);
+        animator.SetFloat("Move Y", direction.y);
     }
 }
