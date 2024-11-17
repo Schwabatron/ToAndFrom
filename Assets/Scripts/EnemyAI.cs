68cdb514307272765f4806002b8c5b43a9e3f0c4
nathan <nstrunk@albany.edu>
2024-11-16 20:00:13 -0500

    Testing drowned spawn

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,47 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private float distance;
+    private Transform nearestPlayer;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        DetectNearestPlayer();
+
+        distance = Vector2.Distance(transform.position, nearestPlayer.transform.position);
+        Vector2 direction = nearestPlayer.transform.position - transform.position;
+        
+        transform.position = Vector2.MoveTowards(this.transform.position, nearestPlayer.transform.position, speed * Time.deltaTime);
+        
+        animator.SetFloat("Move X", direction.x);
+        animator.SetFloat("Move Y", direction.y);
+    }
+
+    void DetectNearestPlayer()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        float closestDistance = Mathf.Infinity;
+        nearestPlayer = null;
+
+        foreach (GameObject player in players)
+        {
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                nearestPlayer = player.transform;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawnPoint.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class EnemySpawnPoint : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+
+    [SerializeField]
+    private float _minimumSpawnTime;
+
+    [SerializeField]
+    private float _maximumSpawnTime;
+
+    private float _timeUntilSpawn;
+
+    void Awake()
+    {
+        SetTimeUntilSpawn();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeUntilSpawn -= Time.deltaTime;
+
+        if (_timeUntilSpawn <= 0)
+        {
+            Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+            SetTimeUntilSpawn();
+        }
+    }
+
+    private void SetTimeUntilSpawn()
+    {
+        _timeUntilSpawn = Random.Range(_minimumSpawnTime, _maximumSpawnTime);
+    }
+}
